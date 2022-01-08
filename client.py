9e6897b238eb0e26907b2032096d1a206f88baee
Lapat-L <lapat.s@ku.th>
2022-01-08 22:00:28 +0700

    add comments and print status response

--- client.py
+++ client.py
@@ -7,42 +7,54 @@ clientSocket = socket(AF_INET, SOCK_DGRAM)
 # start program
 print("-----start game-----")
 name = input("Enter name: ")
-# print('Your balance is 0')
-# print('Please Deposit!')
 
 # send request
 clientSocket.sendto(name.encode(), (serverName, serverPort))
 
 while True:
+    # รับข้อความแสดงสถานะของผ้เล่น
     response, serverAddress = clientSocket.recvfrom(2048)
+
+    # กรณีห้องเต็ม(มีผู้เล่นแล้ว 2 คนใน server) 400
     if(response.decode() == '400'):
         print('Server is full')
         clientSocket.close()
         break
+
+    # แสดงข้อมูลสถานะของผู้เล่น 201
     print(response.decode())
 
+    # รับสถานะเพื่อกำหนดการกระทำลำดับต่อไป
     response, serverAddress = clientSocket.recvfrom(2048)
 
+    # 200 ผู้เล่นครบจำนวนรอผู้เล่น 1 เริ่มการแข่งขัน
     if(response.decode() == '200'):
         start = input('Type start to start game: ')
         clientSocket.sendto(start.encode(), (serverName, serverPort))
+    # 201 รอผู้เล่นคนที่ 1 เริ่มการแข่งขัน
     else:
         print(response.decode())
 
     while True:
+
+        # รับข้อความจาก server เพื่อกำหนดการกระทำต่อไป
         response, serverAddress = clientSocket.recvfrom(2048)
 
+        # 401 เขียน start ไม่ถูกต้อง
         if(response.decode() == '400'):
             start = input('Type start to start game: ')
             clientSocket.sendto(start.encode(), (serverName, serverPort))
             continue
 
+        # 201 ผู้เล่นรอคู่แข่งเลือกตำแหน่ง
         if(response.decode() == '200'):
             print('Wait opponent play ...')
+        # 203 รู้ผลการแข่งขัน
         elif('YOU WIN' in response.decode() or 'YOU LOSE' in response.decode() or 'YOU DRAW' in response.decode()):
             print(name +', '+ response.decode())
             clientSocket.close()
             break
+        # 401 ผู้เล่นใส่ตำแหน่งไม่ถูกต้อง /202 ถึง turn ผู้เล่นเลือกตำแหน่ง 
         else:
             print(response.decode())
             play  = input('Select your placed: ')
--- server.py
+++ server.py
@@ -15,55 +15,102 @@ class Person:
 
 
 print("The server is ready to receive in port:", serverPort)
+# player คือจำนวน request จาก client 
 player = 0
+# address ของผู้เล่นคนที่ 1
 player1 = ''
+# address ของผู้เล่นคนที่ 2
 player2 = ''
+# สถานะของเกม 0 คือยังไม่มีการเริ่มเกม 1 คือมีเกมกำลังดำเนินอยู่
 start = 0
 while True:
     player+=1
+    # รับ request จากผู้เล่นเพื่อเก็บ address
     message, clientAddress = serverSocket.recvfrom(2048)
+
+    # 201 กรณีเป็นผู้เล่นคนที่ 1
     if(player==1):
         player1 = clientAddress
         serverSocket.sendto('You are player 1 (O)\nWaiting for player 2 ...'.encode(), player1)  
+        print(201)
+    # 200/201 กรณีผู้เล่นคนที่ 2
     elif(player==2):
         player2 = clientAddress
         serverSocket.sendto('You are player 2 (X)'.encode(), player2)
+        print(201)
         serverSocket.sendto('200'.encode(), player1)
+        print(200)
         serverSocket.sendto('Wait for player 1 to start game ...'.encode(), player2)
+        print(200)
+    # กรณีผู้เล่นตั้งแต่คนที่ 3 เป็นต้นไป และกรณีรับคำสั่งในระหว่างเล่นเกม
     else:
+        # กรณีผู้ส่ง request เป็นผู้เล่นคนที่ 1 และ 2
         if(clientAddress == player1 or clientAddress == player2):
+            # รับคำสั่ง start จากผู้เล่นคนที่ 1 
             if(message.decode().lower() == 'start' and start == 0):
                 start=1
+                # 201 ส่งสถานะเพื่อแจ้งว่าเกมเริ่มแล้วให้รอผู้เล่นคนที่หนึ่งเลือกวางตำแหน่ง
                 serverSocket.sendto('200'.encode(), player2)
+                print(200)
                 string = '1|2|3\n_ _ _\n4|5|6\n_ _ _\n7|8|9'
-                serverSocket.sendto(string.encode(), player1)  
+                # 202 ส่งข้อมูลเพื่อให้ผู้เล่นคนที่ 1 เลือกตำแหน่งการวาง
+                serverSocket.sendto(string.encode(), player1)
+                print(202) 
+            # 401 กรณีผู้เล่นคนที่เขียน start ผิด  
             elif(message.decode().lower() != 'start' and start == 0):
                 serverSocket.sendto('400'.encode(), player1)
+                print(401)
             else:
+                # ตรวจสอบว่า ตำแหน่งที่รับมาเป็นตัวเลขหรือไม่
                 try:
                     x = message.decode()
                     int(x)
                 except:
-                    continue
-                if(string.count(message.decode())!=1):
+                    # 401 กรณีรับข้อมูลที่ไม่ใช่ตัวเลข
                     if(clientAddress == player1):
-                        out = message.decode()+' are already placed\nPlease placed again.\n'+string
+                        out = message.decode()+' is not a number\nPlease place again.\n'+string
                         serverSocket.sendto(out.encode(), player1)
                     else:
-                        out = message.decode()+' are already placed\nPlease placed again.\n'+string
+                        out = message.decode()+' is not a number\nPlease place again.\n'+string
                         serverSocket.sendto(out.encode(), player2)
+                    print(401)
                     continue
+                # ตรวจสอบว่าตำแหน่งนั้นมีการวางไปหรือยัง
+                if(string.count(message.decode())!=1):
+                    # 401 กรณีที่เลขตำแหน่งไม่ถูกต้อง
+                    if(int(message.decode()) < 0 or int(message.decode()) > 9):
+                        if(clientAddress == player1):
+                            out = message.decode()+' isn\'t number of placed\nPlease place again.\n'+string
+                            serverSocket.sendto(out.encode(), player1)
+                        else:
+                            out = message.decode()+' isn\'t number of placed\nPlease place again.\n'+string
+                            serverSocket.sendto(out.encode(), player2)
+                        print(401)
+                        continue
+                    # 401 กรณีตำแหน่งนั้นได้ถูกวางแล้ว
+                    if(clientAddress == player1):
+                        out = message.decode()+' is already place\nPlease place again.\n'+string
+                        serverSocket.sendto(out.encode(), player1)
+                    else:
+                        out = message.decode()+' is already place\nPlease place again.\n'+string
+                        serverSocket.sendto(out.encode(), player2)
+                    print(401)
+                    continue
+                # ตรวจสอบว่า request มาจากผู้เล่นคนใดเพื่อส่ง response
                 if(clientAddress == player1):
                     string = string.replace(message.decode(), 'O')
                     x = string.count('X')
                     o = string.count('O')
                     total = o+x
+                    # ตรวจสอบผลการแข่งขัน
                     if(string.find('O|O|O') != -1):
                         # player 1 win
                         out1 = 'YOU WIN\n'+string
                         out2 = 'YOU LOSE\n'+string
                         serverSocket.sendto(out1.encode(), player1)
+                        print(203)
                         serverSocket.sendto(out2.encode(), player2)
+                        print(203)
                         start = 0
                         player = 0
                         player1 = ''
@@ -74,7 +121,9 @@ while True:
                         out1 = 'YOU LOSE\n'+string
                         out2 = 'YOU WIN\n'+string
                         serverSocket.sendto(out1.encode(), player1)
+                        print(203)
                         serverSocket.sendto(out2.encode(), player2)
+                        print(203)
                         start = 0
                         player = 0
                         player1 = ''
@@ -85,7 +134,9 @@ while True:
                         out1 = 'YOU WIN\n'+string
                         out2 = 'YOU LOSE\n'+string
                         serverSocket.sendto(out1.encode(), player1)
+                        print(203)
                         serverSocket.sendto(out2.encode(), player2)
+                        print(203)
                         start = 0
                         player = 0
                         player1 = ''
@@ -96,7 +147,9 @@ while True:
                         out1 = 'YOU WIN\n'+string
                         out2 = 'YOU LOSE\n'+string
                         serverSocket.sendto(out1.encode(), player1)
+                        print(203)
                         serverSocket.sendto(out2.encode(), player2)
+                        print(203)
                         start = 0
                         player = 0
                         player1 = ''
@@ -107,7 +160,9 @@ while True:
                         out1 = 'YOU LOSE\n'+string
                         out2 = 'YOU WIN\n'+string
                         serverSocket.sendto(out1.encode(), player1)
+                        print(203)
                         serverSocket.sendto(out2.encode(), player2)
+                        print(203)
                         start = 0
                         player = 0
                         player1 = ''
@@ -118,7 +173,9 @@ while True:
                         out1 = 'YOU LOSE\n'+string
                         out2 = 'YOU WIN\n'+string
                         serverSocket.sendto(out1.encode(), player1)
+                        print(203)
                         serverSocket.sendto(out2.encode(), player2)
+                        print(203)
                         start = 0
                         player = 0
                         player1 = ''
@@ -128,14 +185,19 @@ while True:
                         # draw
                         out = 'YOU DRAW\n'+string
                         serverSocket.sendto(out.encode(), player1)
+                        print(203)
                         serverSocket.sendto(out.encode(), player2)
+                        print(203)
                         start = 0
                         player = 0
                         player1 = ''
                         player2 = ''
                         continue
+                    # หากหลังจากวางตำแหน่งแล้วยังไม่จบการแข่งขันส่ง response ให้ client เพื่อดำเนินเกมต่อ
                     serverSocket.sendto('200'.encode(), player1)
-                    serverSocket.sendto(string.encode(), player2) 
+                    print(201)
+                    serverSocket.sendto(string.encode(), player2)
+                    print(202) 
                 else:
                     string = string.replace(message.decode(), 'X')
                     x = string.count('X')
@@ -146,7 +208,9 @@ while True:
                         out1 = 'YOU WIN\n'+string
                         out2 = 'YOU LOSE\n'+string
                         serverSocket.sendto(out1.encode(), player1)
+                        print(203)
                         serverSocket.sendto(out2.encode(), player2)
+                        print(203)
                         start = 0
                         player = 0
                         player1 = ''
@@ -157,7 +221,9 @@ while True:
                         out1 = 'YOU LOSE\n'+string
                         out2 = 'YOU WIN\n'+string
                         serverSocket.sendto(out1.encode(), player1)
+                        print(203)
                         serverSocket.sendto(out2.encode(), player2)
+                        print(203)
                         start = 0
                         player = 0
                         player1 = ''
@@ -168,7 +234,9 @@ while True:
                         out1 = 'YOU WIN\n'+string
                         out2 = 'YOU LOSE\n'+string
                         serverSocket.sendto(out1.encode(), player1)
+                        print(203)
                         serverSocket.sendto(out2.encode(), player2)
+                        print(203)
                         start = 0
                         player = 0
                         player1 = ''
@@ -179,7 +247,9 @@ while True:
                         out1 = 'YOU WIN\n'+string
                         out2 = 'YOU LOSE\n'+string
                         serverSocket.sendto(out1.encode(), player1)
+                        print(203)
                         serverSocket.sendto(out2.encode(), player2)
+                        print(203)
                         start = 0
                         player = 0
                         player1 = ''
@@ -190,7 +260,9 @@ while True:
                         out1 = 'YOU LOSE\n'+string
                         out2 = 'YOU WIN\n'+string
                         serverSocket.sendto(out1.encode(), player1)
+                        print(203)
                         serverSocket.sendto(out2.encode(), player2)
+                        print(203)
                         start = 0
                         player = 0
                         player1 = ''
@@ -201,7 +273,9 @@ while True:
                         out1 = 'YOU LOSE\n'+string
                         out2 = 'YOU WIN\n'+string
                         serverSocket.sendto(out1.encode(), player1)
+                        print(203)
                         serverSocket.sendto(out2.encode(), player2)
+                        print(203)
                         start = 0
                         player = 0
                         player1 = ''
@@ -211,13 +285,19 @@ while True:
                         # draw
                         out = 'YOU DRAW\n'+string
                         serverSocket.sendto(out.encode(), player1)
+                        print(203)
                         serverSocket.sendto(out.encode(), player2)
+                        print(203)
                         start = 0
                         player = 0
                         player1 = ''
                         player2 = ''
                         continue
                     serverSocket.sendto('200'.encode(), player2)
-                    serverSocket.sendto(string.encode(), player1) 
+                    print(201)
+                    serverSocket.sendto(string.encode(), player1)
+                    print(202) 
+        # 400 กรณีผู้ส่ง request ตั้งแต่อันที่ 3 ที่ไม่ใช่ผู้เล่นเกม
         else:
             serverSocket.sendto('400'.encode(), clientAddress)
+            print(400)
